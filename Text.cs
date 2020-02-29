926f31b5aa6e8dee8da2c901bc31726185d1e411
Asf-artur <asf-artur@yandex.ru>
2020-02-29 21:08:18 +0500

    #master

--- Text.cs
+++ Text.cs
@@ -9,6 +9,24 @@ namespace EngProject
     class Text
     {
         public List<Word> WordsList;
-
+        public Word Get (string s)
+        {
+            foreach (var s1 in WordsList)
+            {
+                if (s1.Meaning == s)
+                    return s1;
+            }
+            return null;
+        }
+        public bool InDict (string s)
+        {
+            foreach (var i in WordsList)
+            {
+                if (i.Meaning == s)
+                    return true;
+            }
+            return false;
+           
+        }
     }
 }
